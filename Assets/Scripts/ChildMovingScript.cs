3dee3e9be1f71ca9f71a9f0e86589b55d5be0f49
高山瑞基 <>
2020-08-20 01:30:32 +0900

    ぶつかった相手のタグを判別できるように →Destroyしたアイテムを表示させる方法を模索中

--- Assets/Scripts/ChildMovingScript.cs
+++ Assets/Scripts/ChildMovingScript.cs
@@ -35,4 +35,12 @@ public class ChildMovingScript : MonoBehaviour
 
         child_pos = transform.position; //プレイヤーの位置を更新
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag =="Item")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
