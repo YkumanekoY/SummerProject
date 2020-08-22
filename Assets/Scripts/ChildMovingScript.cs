db12fe91dd9d15577a076611371c4134027b3a7b
高山瑞基 <>
2020-08-22 16:59:14 +0900

    ・アイテム管理のリストを導入 ・取得したアイテムの画像がアイテム欄に表示されるように

--- Assets/Scripts/ChildMovingScript.cs
+++ Assets/Scripts/ChildMovingScript.cs
@@ -8,8 +8,11 @@ public class ChildMovingScript : MonoBehaviour
     private float x; //x方向のImputの値
     private float y; //y方向のInputの値
     private Rigidbody2D rigd;
-    float childSpeed = 5.0f;
-
+    float childSpeed = 5.0f;// 子供のスピード
+    private bool escape = true;
+    public GameObject childPrefab;
+    [SerializeField]
+    Transform itemList;
 
     // Start is called before the first frame update
     void Start()
@@ -21,26 +24,28 @@ public class ChildMovingScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(escape == true)
+        {
         x = Input.GetAxis("Horizontal"); //x方向のInputの値を取得
         y = Input.GetAxis("Vertical"); //z方向のInputの値を取得
 
         rigd.velocity = new Vector2(x * childSpeed, y * childSpeed); //プレイヤーのRigidbodyに対してInputにspeedを掛けた値で更新し移動
 
-        Vector2 diff = (transform.position - child_pos).normalized; //プレイヤーがどの方向に進んでいるかがわかるように、初期位置と現在地の座標差分を取得
-
-        if (diff.magnitude > 0.01f) //ベクトルの長さが0.01fより大きい場合にプレイヤーの向きを変える処理を入れる(0では入れないので）
-        {
-            transform.rotation = Quaternion.FromToRotation(Vector3.up, diff);  //ベクトルの情報をQuaternion.LookRotationに引き渡し回転量を取得しプレイヤーを回転させる
-        }
-
         child_pos = transform.position; //プレイヤーの位置を更新
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag =="Item")
+        if (collision.gameObject.tag == "Item")
         {
             Destroy(collision.gameObject);
+            Instantiate(childPrefab, transform.position, Quaternion.identity, itemList);
+        }
+        if(collision.gameObject.tag == "Enemy")
+        {
+            rigd.bodyType = RigidbodyType2D.Kinematic;
+            escape = false;
         }
     }
 }
