1c96baae5a23df34afaf5855e1d0baa156e1089a
YkumanekoY <new_address_for_kumanekoy@gmail.com>
2020-08-23 21:14:41 +0900

    修正

--- Assets/Scripts/BasicSystem/MainMap_UIManager.cs
+++ Assets/Scripts/BasicSystem/MainMap_UIManager.cs
@@ -15,7 +15,7 @@ public class MainMap_UIManager : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-		countDownLabel.text = "3";
+		countDownLabel.text = "";
 		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 	}
 
