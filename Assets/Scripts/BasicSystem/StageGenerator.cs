f8f672adb80d420d28a52b4aa1d2ccf56706593d
YkumanekoY <new_address_for_kumanekoy@gmail.com>
2020-08-23 10:44:51 +0900

    建物の自動生成　進行中
    建物の中身がランダムに変わる仕様完了

---
+++ Assets/Scripts/BasicSystem/StageGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class StageGenerator : MonoBehaviour
+{
+	public enum BuildingType
+	{
+		A
+	}
+
+	[SerializeField] GameObject parent;
+
+	[SerializeField]
+	private GameObject[]
+		roof,
+		wall,
+		insideObjects,
+		floor;
+
+	private void Awake()
+	{
+		for (int i = 0; i < 1; i++)
+		{
+			Transform temp = Instantiate(parent, transform.position + Vector3.right * 10 * i, Quaternion.identity).transform;
+
+			Vector3 tPos = temp.position;
+			GameObject r = Instantiate(roof[Random.Range(0, roof.Length)], tPos, Quaternion.identity, temp);
+			GameObject w = Instantiate(wall[Random.Range(0, wall.Length)], tPos, Quaternion.identity, temp);
+			GameObject insideO = Instantiate(insideObjects[Random.Range(0, insideObjects.Length)], tPos, Quaternion.identity, temp);
+			GameObject f = Instantiate(floor[Random.Range(0, floor.Length)], tPos, Quaternion.identity, temp);
+
+			temp.gameObject.GetComponent<BuildingScript>().SetComponents(r, w, insideO, f);
+		}
+	}
+
+	void Start()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Map/BuildingScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingScript : MonoBehaviour
+{
+	StageGenerator.BuildingType buildingType;
+
+	[SerializeField]
+	private GameObject
+		roof,
+		wall,
+		insideObject,
+		floor;
+
+	public void SetComponents(
+		GameObject r,
+		GameObject w,
+		GameObject insideO,
+		GameObject f)
+	{
+		roof = r;
+		wall = w;
+		insideObject = insideO;
+		floor = f;
+
+		insideObject.SetActive(false);
+	}
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		Debug.Log("atack");
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("in");
+		if (other.CompareTag("Player"))
+		{
+			roof.SetActive(false);
+			insideObject.SetActive(true);
+		}
+	}
+	void OnTriggerExit2D(Collider2D other)
+	{
+		Debug.Log("out");
+		if (other.CompareTag("Player"))
+		{
+			roof.SetActive(true);
+			insideObject.SetActive(false);
+		}
+	}
+}
