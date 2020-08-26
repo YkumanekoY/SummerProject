5a0e5dcbb4cb5ed6dfa6b9a5d0d5ec4aed9b80a5
YkumanekoY <new_address_for_kumanekoy@gmail.com>
2020-08-26 23:56:33 +0900

    封印の場所作った

---
+++ Assets/Scripts/BasicSystem/ItemManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+	public enum ItemName
+	{
+		SealedCharm, revivalCharm
+	}
+
+	public GameObject[] ItemPrefabs;
+}
