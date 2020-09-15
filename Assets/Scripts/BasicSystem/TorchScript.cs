0899f9240998f27175a73f7025384015a9feaa1b
Mizuki Takayama <miz39t@gmail.com>
2020-09-15 11:31:32 +0900

    コルーチンでライトのインターバルを設定した

--- Assets/Scripts/BasicSystem/TorchScript.cs
+++ Assets/Scripts/BasicSystem/TorchScript.cs
@@ -9,9 +9,11 @@ public class TorchScript : MonoBehaviour
     Vector2 direction;
     GameObject EnemyGameObject;
     KidnappingScript kidnappingScript;
-    float torchInterval = 3;
+    float torchIntervalTime = 5;
     private Rigidbody rigd;
     private Vector3 Player_pos; //プレイヤーのポジション
+    float torchActiveTime = 3;
+    bool torchActive = true; //トーチの使用可能の有無
 
     // Start is called before the first frame update
     void Start()
@@ -24,17 +26,20 @@ public class TorchScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(torchActive == true)
         {
-            childDirection = Vector3.Distance(EnemyGameObject.transform.position, transform.position);// 敵との距離を把握
-          
-            if (childDirection < 5f)
+          if (Input.GetKeyDown(KeyCode.Space))
             {
-                GameObject.Find("TorchManager").transform.Find("Torch").gameObject.SetActive(true);
+                childDirection = Vector3.Distance(EnemyGameObject.transform.position, transform.position);// 敵との距離を把握
+        
+                if (childDirection < 5f)
+                {
+                    GameObject.Find("TorchManager").transform.Find("Torch").gameObject.SetActive(true);
                 
-                //秒数のコルーチンを開始
-                StartCoroutine("torchCount");   
-                kidnappingScript.AttackedbyLight(childGameObjectNumber);
+                    //秒数のコルーチンを開始
+                    StartCoroutine("torchCount");   
+                    kidnappingScript.AttackedbyLight(childGameObjectNumber);
+                }
             }
         }
 
@@ -44,11 +49,24 @@ public class TorchScript : MonoBehaviour
     IEnumerator torchCount()
     {
         //インターバル分停止させる
-        yield return new WaitForSeconds(torchInterval);
+        yield return new WaitForSeconds(torchActiveTime);
 
         //再びライトをオフに
         GameObject.Find("TorchManager").transform.Find("Torch").gameObject.SetActive(false);
 
+        StartCoroutine("torchInterval");
+        torchActive = false;
+
     }
+
+    //トーチのインターバルを設けるメソッド
+    IEnumerator torchInterval()
+    {
+        yield return new WaitForSeconds(torchIntervalTime);
+
+        //再びライトが使えるように
+        torchActive = true;
+    }
+
 
 }
