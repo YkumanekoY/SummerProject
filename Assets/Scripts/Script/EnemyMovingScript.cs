54749c28ca41510965532af587d69e3b5b6bedd0
YkumanekoY <new_address_for_kumanekoy@gmail.com>
2020-09-18 23:42:07 +0900

    コンフリクトを直した

--- Assets/Scripts/Script/EnemyMovingScript.cs
+++ Assets/Scripts/Script/EnemyMovingScript.cs
@@ -13,14 +13,10 @@ public class EnemyMovingScript : MonoBehaviourPunCallbacks
     Transform itemList;
     GameManager gameManager;
     GameObject gameManagerObj;
-<<<<<<< HEAD
     [SerializeField]
     Camera enemyCamera;
     TransformingScript transformingScript;
 	[SerializeField] GameObject itemListObj;
-=======
-    GameObject itemListObj;
->>>>>>> 31d21da39d677211c7ecabe5ac78b97d4f8ede50
     float inputX; //x方向のImputの値
     float inputY; //y方向のInputの値
 
@@ -30,7 +26,6 @@ public class EnemyMovingScript : MonoBehaviourPunCallbacks
 
     void Start()
     {
-<<<<<<< HEAD
         if (photonView.IsMine)
         {
             gameManagerObj = GameObject.Find("GameManager");
@@ -44,17 +39,6 @@ public class EnemyMovingScript : MonoBehaviourPunCallbacks
             itemListObj.gameObject.SetActive(true);
             enemyCamera.gameObject.SetActive(true);
         }
-
-=======
-        gameManagerObj = GameObject.Find("GameManager");
-        gameManager = gameManagerObj.GetComponent<GameManager>();
-        itemListObj = GameObject.Find("ItemList");//名前違うかもしれん
-
-        this.rigidBody = GetComponent<Rigidbody2D>();
-        // 衝突時にobjectを回転させない設定
-        this.rigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;
-        currentSpeed = childSpeed;
->>>>>>> 31d21da39d677211c7ecabe5ac78b97d4f8ede50
     }
 
     private void Update()
@@ -90,7 +74,6 @@ public class EnemyMovingScript : MonoBehaviourPunCallbacks
     {
         if (photonView.IsMine)
         {
-<<<<<<< HEAD
             if (collider.gameObject.tag == "Item" && Input.GetKeyDown(KeyCode.Return))
             {
                 itemPoint = collider.gameObject.GetComponent<ItemPoint>();
@@ -99,16 +82,9 @@ public class EnemyMovingScript : MonoBehaviourPunCallbacks
 
             if (!transformingScript.isGhostLooking) return;
             if (collider.gameObject.tag == "Item" && Input.GetKeyDown(KeyCode.B))
-=======
-            collider.gameObject.GetComponent<ItemPoint>().SearchItem(this.gameObject);
-            itemPoint = collider.gameObject.GetComponent<ItemPoint>();
-                
-            if(itemListObj.transform.childCount != 0) //リストになんかアイテムがあったら
->>>>>>> 31d21da39d677211c7ecabe5ac78b97d4f8ede50
             {
                 if (itemListObj.transform.childCount != 0) //リストになんかアイテムがあったら
                 {
-<<<<<<< HEAD
                     if (itemPoint.isItemPut())
                     {
                         if (itemListObj.transform.GetChild(0).gameObject.tag == "SealedCharm")
@@ -118,19 +94,6 @@ public class EnemyMovingScript : MonoBehaviourPunCallbacks
 
                         Destroy(itemListObj.transform.GetChild(0).gameObject);
                     }
-=======
-                    if(itemListObj.transform.GetChild(0).gameObject.tag == "SealedCharm")
-                    {
-                        itemPoint.HidingItem("SealedCharm");
-                        Destroy(itemListObj.transform.GetChild(0).gameObject);
-                    }
-                    else if(itemListObj.transform.GetChild(0).gameObject.tag == "RevivalCharm")
-                    {
-                        itemPoint.HidingItem("RevivalCharm");
-                        Destroy(itemListObj.transform.GetChild(0).gameObject);
-                    }
-                    
->>>>>>> 31d21da39d677211c7ecabe5ac78b97d4f8ede50
                 }
             }
         }
